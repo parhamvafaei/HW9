0124a00c63545bba36651521146160e1889f4563
parham <p.vafaeei@gamil.com>
2022-08-20 14:10:23 +0430

    add toString method to Admin

--- src/main/java/org/example/entity/Admin.java
+++ src/main/java/org/example/entity/Admin.java
@@ -24,4 +24,9 @@ public class Admin {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
