6ddde1ca8fcde5eb241b3a3785460357bc60d3c1
parham <p.vafaeei@gamil.com>
2022-08-20 14:00:18 +0430

    Set Idea settings ,create DBSconfig

---
+++ src/main/java/org/example/config/DBConfig.java
@@ -0,0 +1,22 @@
+package org.example.config;
+
+
+    import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+    public class DBConfig {
+        private static Connection connection;
+        static {
+            try {
+                connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/databasemaktab","postgres","parham218");
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        public static Connection getConnection() {
+            return connection;
+        }
+    }
+
