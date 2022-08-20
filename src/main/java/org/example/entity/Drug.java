8203dde78c54712b07e635c6b69acdeac8afc34d
parham <p.vafaeei@gamil.com>
2022-08-20 14:15:44 +0430

    create Drug class and setter getter

---
+++ src/main/java/org/example/entity/Drug.java
@@ -0,0 +1,100 @@
+package org.example.entity;
+
+import java.util.Objects;
+
+public class Drug {
+    private String name;
+    private int quantity;
+    private long price;
+    private boolean doesExist;
+    private boolean isConfirm;
+
+    private long totalPrice;
+    private String patientNationalCode;
+    public Drug(String name, int quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public void setPrice(long price) {
+        this.price = price;
+    }
+
+    public boolean isDoesExist() {
+        return doesExist;
+    }
+
+    public void setDoesExist(boolean doesExist) {
+        this.doesExist = doesExist;
+    }
+
+    public long getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice() {
+        totalPrice = price*quantity;
+    }
+
+    public String getPatientNationalCode() {
+        return patientNationalCode;
+    }
+
+    public void setPatientNationalCode(String patientNationalCode) {
+        this.patientNationalCode = patientNationalCode;
+    }
+
+    public boolean isConfirm() {
+        return isConfirm;
+    }
+
+    public void setConfirm(boolean confirm) {
+        isConfirm = confirm;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return  "name='" + name + '\'' +
+                ", quantity=" + quantity +
+                ", price=" + price +
+                ", doesExist=" + doesExist +
+                ", isConfirmed=" + isConfirm+
+                ", totalPrice=" + totalPrice +
+                ", patient's national code=" + patientNationalCode;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Drug that = (Drug) o;
+        return Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
