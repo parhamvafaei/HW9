d446caa8d51634f1f7c2576871907f57d79d1141
parham <p.vafaeei@gamil.com>
2022-08-20 14:38:47 +0430

    Create AdminRepository in package repository and create methods c r a t

---
+++ src/main/java/org/example/repository/AdminRepository.java
@@ -0,0 +1,11 @@
+package org.example.repository;
+import org.example.entity.Admin;
+import java.sql.SQLException;
+public interface AdminRepository {
+    void save(Admin admin) throws SQLException;
+    Admin load(String username, String password) throws SQLException;
+    boolean loadByUsername(String username) throws SQLException;
+    void changePassword(String username, String oldPassword, String newPassword) throws SQLException;
+    void changeUsername(String oldUsername, String newUsername, String password) throws SQLException;
+
+}
